17565c42d7b4a37876a4f02a503b9415ea9b0775
Jacob Carrington <jacob.john.carrington@gmail.com>
2020-08-24 11:50:46 +1200

    Update

--- gatsby-theme-auth0/src/auth/config.ts
+++ gatsby-theme-auth0/src/auth/config.ts
@@ -1,17 +1,21 @@
 const config = {
-    domain: process.env.AUTH0_DOMAIN!,
-    clientID: process.env.AUTH0_CLIENT_ID!,
-    redirectUri: process.env.AUTH0_CALLBACK_URL!,
-    audience: process.env.AUTH0_AUDIENCE!,
-    responseType: process.env.AUTH0_RESPONSE_TYPE! || "token id_token",
-    scope: process.env.AUTH0_SCOPE! || "openid email profile",
-}
+  domain: process.env.AUTH0_DOMAIN!,
+  clientID: process.env.AUTH0_CLIENT_ID!,
+  redirectUri: process.env.AUTH0_CALLBACK_URL!,
+  audience: process.env.AUTH0_AUDIENCE!,
+  responseType: process.env.AUTH0_RESPONSE_TYPE! || "token id_token",
+  scope: process.env.AUTH0_SCOPE! || "openid email profile",
+  overrides: {},
+};
 
-if(process.env.AUTH0_TOKEN_ISSUER !== undefined && process.env.AUTH0_TOKEN_ISSUER !== undefined) {
+if (
+  process.env.AUTH0_TOKEN_ISSUER !== undefined &&
+  process.env.AUTH0_TOKEN_ISSUER !== undefined
+) {
   config.overrides = {
-    __tenant: process.env.AUTH0_TENANT!,
-    __token_issuer: process.env.AUTH0_TOKEN_ISSUER!
-  }
+    __tenant: process.env.AUTH0_TENANT!, // eslint-disable-line @typescript-eslint/camelcase
+    __token_issuer: process.env.AUTH0_TOKEN_ISSUER!, // eslint-disable-line @typescript-eslint/camelcase
+  };
 }
 
-export default config
+export { config };
